ea4ca20a00b63673fcc26a38384c270e9a4e2188
dlwortmann <dannywortmann@gmail.com>
2025-04-02 19:32:11 -0500

    updated models, schema, and env

--- server/src/models/book.ts
+++ server/src/models/book.ts
@@ -1,5 +1,7 @@
+import { DataTypes,  type Sequelize, Model, type Optional } from 'sequelize';
+
 interface BookAttributes {
-    isbn: number
+    isbn: number;
     title: string;
     author: string;
     publishDate: string;
@@ -7,11 +9,68 @@ interface BookAttributes {
     genre: string;
     description: string;
     isMovie?: boolean;
-    reviews: string[];
+    reviews?: string[];
 }
 
 interface BookCreationAttributes extends Optional<BookAttributes, 'isbn'> {}
 
 export class Book extends Model<BookAttributes, BookCreationAttributes> implements BookAttributes {
-    
+    public isbn!: number;
+    public title!: string;
+    public author!: string;
+    public publishDate!: string;
+    public pageCount!: number;
+    public genre!: string;
+    public description!: string;
+    public isMovie?: boolean | undefined;
+    public reviews!: string[];
+}
+
+export function BookFactory(sequelize: Sequelize):
+ typeof Book {
+    Book.init(
+        {
+            isbn: {
+                type: DataTypes.INTEGER,
+                allowNull: false,
+            },
+            title: {
+                type: DataTypes.STRING,
+                allowNull: false,
+            },
+            author: {
+                type: DataTypes.STRING,
+                allowNull: false,
+            },
+            publishDate: { 
+                type: DataTypes.STRING,
+                allowNull: false,
+            },
+            pageCount: { 
+                type: DataTypes.INTEGER,
+                allowNull: false,
+            },
+            genre: { 
+                type: DataTypes.STRING,
+                allowNull: false,
+            },
+            description: { 
+                type: DataTypes.STRING,
+                allowNull: false,
+            },
+            isMovie: { 
+                type: DataTypes.BOOLEAN,
+                allowNull: true,
+            },
+            reviews: { 
+                type: DataTypes.ARRAY,
+                allowNull: true,
+            }
+        },
+        {
+            tableName: 'books',
+            sequelize,
+        }
+    )
+    return Book
 }
--- server/src/models/movie.ts
+++ server/src/models/movie.ts
@@ -1,3 +1,65 @@
+import { DataTypes, Sequelize, Model, Optional } from 'sequelize';
+
+
 interface MovieAttributes {
-    id: number
+    title: string;
+    director: string;
+    actors: string;
+    released: string;
+    poster: string;
+    genre: string;
+    plot: string;
 }
+
+interface MovieCreationAttributes extends Optional<MovieAttributes, 'title'> { }
+
+export class Movie extends Model<MovieAttributes, MovieCreationAttributes> implements MovieAttributes {
+    public title!: string;
+    public director!: string;
+    public actors!: string;
+    public released!: string;
+    public poster!: string;
+    public genre!: string;
+    public plot!: string;
+}
+
+export function MovieFactory(sequelize: Sequelize): typeof Movie {
+    Movie.init(
+        {
+            title: {
+                type: DataTypes.STRING,
+                allowNull: false,
+            },
+            director: {
+                type: DataTypes.STRING,
+                allowNull: false,
+            }, 
+            actors: {
+                type: DataTypes.STRING,
+                allowNull: false,
+            },
+            released: {
+                type: DataTypes.STRING,
+                allowNull: false,
+            },
+            poster: {
+                type: DataTypes.STRING,
+                allowNull: false,
+            },
+            genre: {
+                type: DataTypes.STRING,
+                allowNull: false,
+            },
+            plot: {
+                type: DataTypes.STRING,
+                allowNull: false,
+            },
+        },
+        {
+            tableName: 'movies',
+            sequelize,
+        }
+    )
+    return Movie
+}
+
