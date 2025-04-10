0eee22518af11d1d3cf4ba7ffd29c7d366b1fef5
DeJohn Nelson <dejohn.nelson1998gmail.com>
2025-04-09 20:10:11 -0500

    added login button navigation

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,8 @@
 import Header from './components/Header';
 import Footer from './components/Footer';
 import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 
 function App() {
   
